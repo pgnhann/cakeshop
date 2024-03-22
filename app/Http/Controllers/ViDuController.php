5f8f3739b5d8ed98d33ab9e7274c9c256597f948
pgnhann <phuongnhan00.w@gmail.com>
2024-03-22 08:17:17 +0700

    create project

---
+++ app/Http/Controllers/ViDuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViDuController extends Controller
+{
+    //
+    function vidu()
+    {
+        return view("vidu1");
+    }
+}
